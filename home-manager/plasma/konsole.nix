5f3a149c423f7e674cd2a5982913a737f7dd8f22
joserodriguesjr <joseluisrjunior@gmail.com>
2025-06-30 12:28:34 -0300

    konsole plasma styling

--- home-manager/plasma/konsole.nix
+++ home-manager/plasma/konsole.nix
@@ -10,12 +10,19 @@
   programs.plasma = {
     enable = true;
 
+    # /home/USER/.config
     configFile."konsolerc" = {
       "Desktop Entry" = {
         DefaultProfile = "zsh.profile";
       };
+
+# Already defined inside zsh.profile
+#      "UiSettings" = {
+#        ColorScheme = "NixOs";
+#      };
     };
 
+    # /home/USER/.local/share
     dataFile."konsole/zsh.profile" = {
       "General" = {
         Name = "Zsh";
@@ -24,32 +31,64 @@
       };
 
       "Appearance" = {
-        ColorScheme = "Transparent";
+        ColorScheme = "NixOs";
       };
     };
 
-    dataFile."konsole/Transparent.colorscheme" = {
-      "Background" = {
-        Color = "0,0,0"; # black
-        Transparency = 40; # 0–255; lower = more transparent
-      };
+    dataFile."konsole/NixOs.colorscheme" = {
+      "Background" = { Color = "35,38,39"; };
+      "BackgroundFaint" = { Color = "49,54,59"; };
+      "BackgroundIntense" = { Color = "0,0,0"; };
 
-      "Foreground" = {
-        Color = "255,255,255";
-      };
+      "Color0" = { Color = "35,38,39"; };
+      "Color0Faint" = { Color = "49,54,59"; };
+      "Color0Intense" = { Color = "127,140,141"; };
+
+      "Color1" = { Color = "237,21,21"; };
+      "Color1Faint" = { Color = "120,50,40"; };
+      "Color1Intense" = { Color = "192,57,43"; };
+
+      "Color2" = { Color = "17,209,22"; };
+      "Color2Faint" = { Color = "23,162,98"; };
+      "Color2Intense" = { Color = "28,220,154"; };
+
+      "Color3" = { Color = "246,116,0"; };
+      "Color3Faint" = { Color = "182,86,25"; };
+      "Color3Intense" = { Color = "253,188,75"; };
+
+      "Color4" = { Color = "29,153,243"; };
+      "Color4Faint" = { Color = "27,102,143"; };
+      "Color4Intense" = { Color = "61,174,233"; };
+
+      "Color5" = { Color = "155,89,182"; };
+      "Color5Faint" = { Color = "97,74,115"; };
+      "Color5Intense" = { Color = "142,68,173"; };
+
+      "Color6" = { Color = "26,188,156"; };
+      "Color6Faint" = { Color = "24,108,96"; };
+      "Color6Intense" = { Color = "22,160,133"; };
+
+      "Color7" = { Color = "252,252,252"; };
+      "Color7Faint" = { Color = "99,104,109"; };
+      "Color7Intense" = { Color = "255,255,255"; };
+
+      "Foreground" = { Color = "252,252,252"; };
+      "ForegroundFaint" = { Color = "239,240,241"; };
+      "ForegroundIntense" = { Color = "61,174,233"; };
 
       "General" = {
+        Anchor="0.5,0.5";
         Blur = true;
         Opacity = 0.75;
         ColorRandomization=false;
-        #FillStyle=Tile;
+        FillStyle="Tile";
 
-        #Wallpaper=;
-        #WallpaperFlipType=NoFlip;
-        #WallpaperOpacity=1;
+        Wallpaper="";
+        WallpaperFlipType="NoFlip";
+        WallpaperOpacity="1";
 
-        Description = "Transparent NixOS";
-        ColorScheme = "Transparent";
+        Description = "NixOS - Breeze + Transparent";
+        ColorScheme = "NixOs";
       };
     };
   };
