8fd9c295e3df13acb1aec5a6b976973d550cb408
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-06 15:41:19 -0300

    remove 30 fps cap

--- kernel/gaming/rpcs3.nix
+++ kernel/gaming/rpcs3.nix
@@ -1,6 +1,8 @@
 # https://www.playstation.com/en-us/support/hardware/ps3/system-software/
 # Roms: https://romsfun.com/roms/ps3
 
+# Remove 30 fps cap: https://www.youtube.com/watch?v=jNoyo1XXAus&pp=ygUGI3IweDFj
+
 { pkgs, ... }:
 
 {
