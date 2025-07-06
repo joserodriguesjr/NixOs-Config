96c316d3c221f94134194fdc67bf2307ea6695e4
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-06 00:02:57 -0300

    gaming packages

--- kernel/kernel.nix
+++ kernel/kernel.nix
@@ -20,10 +20,19 @@
     bitwarden-desktop
 
     # Gaming
+    # https://www.protondb.com/
     mangohud # A Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more.
+    gamemode
     protonup-qt # Install Wine and Proton-based Compatibility Tools
-    lutris # Play all your games on Linux
-    bottles # Run Windows software on Linux 
     heroic # Open Source Launcher for GOG, Epic Games and Amazon Games
+
+    mesa
+    vulkan-headers
+    vulkan-loader
+    
+    # lutris # Play all your games on Linux
+    # bottles # Run Windows software on Linux 
+    # wine
+    # winetricks
   ];
 }
