070b7de98c1cac2f7fd0dce417426ed9064da910
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-05 21:32:06 -0300

    adding doc

--- kernel/gaming/steam.nix
+++ kernel/gaming/steam.nix
@@ -2,6 +2,7 @@
   # https://github.com/FeralInteractive/gamemode
   programs.gamemode.enable = true;
 
+  # https://nixos.wiki/wiki/Steam#Install
   programs.steam ={
     enable = true;
     gamescopeSession.enable = true;
