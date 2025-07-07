fe232bf41b72a9ec4ce15a084a98a54bb2d8594b
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-07 19:29:45 -0300

    organize

--- kernel/hardware/network.nix
+++ kernel/hardware/network.nix
@@ -1,24 +1,27 @@
 { lib, ... }:
 
 {
-  networking.hostName = "nixos"; # Define your hostname.
+  networking = {
+    hostName = "nixos";
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    useDHCP = lib.mkDefault false;
+
+    # Enable networking
+    networkmanager.enable = true;
+    # Enables wireless support via wpa_supplicant.
+    # wireless.enable = true;
+  };
+
   # networking.interfaces.enp58s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # Enables wireless support via wpa_supplicant.
-  # networking.wireless.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
