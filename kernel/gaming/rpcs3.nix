3183f28ca1d405c5861ab9482f917d949adf7477
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-07 16:39:50 -0300

    docs

--- kernel/gaming/rpcs3.nix
+++ kernel/gaming/rpcs3.nix
@@ -1,4 +1,5 @@
 # https://www.playstation.com/en-us/support/hardware/ps3/system-software/
+# https://rpcs3.net/quickstart
 # Roms: https://romsfun.com/roms/ps3
 
 # Remove 30 fps cap: https://www.youtube.com/watch?v=jNoyo1XXAus&pp=ygUGI3IweDFj
