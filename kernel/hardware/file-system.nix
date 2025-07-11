4490680780dc31941dbd532afe80b2d4f05eae0d
joserodriguesjr <joseluisrjunior@gmail.com>
2025-07-11 19:25:54 -0300

    new device

--- kernel/hardware/file-system.nix
+++ kernel/hardware/file-system.nix
@@ -4,6 +4,12 @@
     fsType = "ext4";
   };
 
+  fileSystems."/games" = {
+    device = "/dev/disk/by-uuid/3dcbbb22-78ee-46b3-b36d-4daee03af670";
+    fsType = "ext4";
+    neededForBoot = false;
+  };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/B934-4EA3";
     fsType = "vfat";
@@ -15,5 +21,7 @@
 
   swapDevices = [
     { device = "/dev/disk/by-uuid/86d200ca-f473-4b74-a65e-fae9a6e2edfc"; }
+    # size = 8 * 1024; # 8GB
   ];
+  # swapDevices = lib.mkForce [ ]; # Remove all swap devices
 }
